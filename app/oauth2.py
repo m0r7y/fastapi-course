2cb775b551f5b1163bffa2114b72f80e65704a43
m0r7y <r.radoniaina11@gmail.com>
2023-04-05 12:27:01 +0200

    fix typo

--- app/oauth2.py
+++ app/oauth2.py
@@ -21,7 +21,7 @@ def create_access_token(data: dict):
     to_encode = data.copy()
 
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp": expire})
+    to_encode.update({'exp': expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
@@ -33,7 +33,7 @@ def verify_access_token(token: str, credentials_exception):
     try:
 
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id: str = payload.get("user_id")
+        id: str = payload.get('user_id')
         if id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id)
@@ -43,9 +43,14 @@ def verify_access_token(token: str, credentials_exception):
     return token_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                          detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f'Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
 
     token = verify_access_token(token, credentials_exception)
 
